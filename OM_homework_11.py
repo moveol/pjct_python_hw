5684a2d3699a8fbafd7801a689a0c077fc0821e6
moveol <124434693+moveol@users.noreply.github.com>
2023-03-21 19:23:23 +0200

    Hw_11

---
+++ OM_homework_11.py
@@ -0,0 +1,11 @@
+def cat_with_hats(number_of_cats: int, number_of_rounds: int):
+    cat_list = [False] * number_of_cats  # List of each cat's status hat. at the start none of them have
+    for round_number in range(1, number_of_rounds+1):
+        for hat in range(round_number-1, number_of_cats, round_number):
+            cat_list[hat] = not cat_list[hat]  # Change the hat status
+    return [hat+1 for hat in range(number_of_cats) if cat_list[hat]]
+
+
+number_of_cats = 100
+number_of_rounds = 100
+print("Cats with following numbers have hats: ", cat_with_hats(number_of_cats, number_of_rounds))
