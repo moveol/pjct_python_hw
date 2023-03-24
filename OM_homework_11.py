1a22455cd68fc542e3237e1b3a6225aea6685390
moveol <124434693+moveol@users.noreply.github.com>
2023-03-25 01:43:38 +0200

    Update OM_homework_11.py

--- OM_homework_11.py
+++ OM_homework_11.py
@@ -1,9 +1,19 @@
-def cat_with_hats(number_of_cats: int, number_of_rounds: int):
+def cat_with_hats(number_of_cats: int, number_of_rounds: int) -> List[int]:
+    '''
+    Returns the list of cats with hats after a certain number of rounds.
+
+    Parameters:
+    number_of_cats (int): The number of cats.
+    number_of_rounds (int): The number of rounds.
+
+    Returns:
+    List[int]: The list of cats with hats.
+    '''
     cat_list = [False] * number_of_cats  # List of each cat's status hat. at the start none of them have
-    for round_number in range(1, number_of_rounds+1):
-        for hat in range(round_number-1, number_of_cats, round_number):
+    for round_number in range(1, number_of_rounds + 1):
+        for hat in range(round_number - 1, number_of_cats, round_number):
             cat_list[hat] = not cat_list[hat]  # Change the hat status
-    return [hat+1 for hat in range(number_of_cats) if cat_list[hat]]
+    return [hat + 1 for hat in range(number_of_cats) if cat_list[hat]]
 
 
 number_of_cats = 100
